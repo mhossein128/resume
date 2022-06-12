091c90e345ceb9cf4a088ef2ce41df080387dd34
mhossin <mohammadimh287@gmail.com>
2022-06-12 19:20:30 +0430

    delete input input

--- script.js
+++
@@ -1,8 +0,0 @@
-let file = document.querySelector('#file')
-
-file.addEventListener('change',(e)=>{
-    let file =  e.target.files[0]
-    let url = URL.createObjectURL(file)
-    console.log(url.slice(5))
-
-})
